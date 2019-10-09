ac45d0b447ecc0f4d3b3cc66ba39d2951c709c6d
saurabh thareja <saurabhthareja90@gmail.com>
2019-10-09 14:24:37 +1100

    added serverspec test file

---
+++ spec/Dockerfile_spec.rb
@@ -0,0 +1,22 @@
+require "serverspec"
+require "docker"
+
+describe "Dockerfile" do
+  before(:all) do
+    @image = Docker::Image.build_from_dir('.')
+    set :os, family: :alpine
+    set :backend, :docker
+    set :docker_image, @image.id
+    set :docker_container_create_options, { 'Entrypoint' => ['ash'] }
+  end
+
+  it "should execute entrypoint" do
+    expect(@image.json["Config"]["Entrypoint"][0]).to eq("docker-entrypoint.sh")
+  end
+  
+  it "should pass the -h option by default" do
+    expect(@image.json["Config"]["Cmd"][0]).to eq("npm")
+     expect(@image.json["Config"]["Cmd"][1]).to eq("start")
+  end
+
+end
